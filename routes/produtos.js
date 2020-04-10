685c6e1c73142b46cf6c9d7b29b41d09f0100f26
leandrooriguela <leandrooriguela@hotmail.com>
2020-04-10 11:37:52 -0300

    Implementado Boas Práticas

--- routes/produtos.js
+++ routes/produtos.js
@@ -8,35 +8,56 @@ router.get('/', (req, res, next) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
             'SELECT * FROM produtos;',
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({response: resultado})
+                const response = {
+                    quantidade: result.length,
+                    produtos: result.map(prod => {
+                        return {
+                            id_produto: prod.id_produto,
+                            nome: prod.nome,
+                            preco: prod.preco,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna os detalhes de um produto específico',
+                                url: 'http://localhost:3000/produtos/' + prod.id_produto
+                            }
+                        }
+                        })
+                }
+                return res.status(200).send({response});
             }
         )
-    })
+    });
 });
 
 //Insere um Produto --POST
 router.post('/', (req, res, next) => {
-    
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) } 
         conn.query(
             'INSERT INTO produtos (nome, preco) VALUES (?,?)',
             [req.body.nome, req.body.preco],
-            (erro, resultado, field) => {
+            (erro, result, field) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(201).send({
-                    mensagem: 'Produto inserido com sucesso',
-                    id_produto: resultado.insertId
-                });
+                const response = {
+                    mensagem: 'Porduto inserido com sucesso',
+                    produtoCriado: {
+                        id_produto: result.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Insere um produto',
+                            url: 'http://localhost:3000/produtos'
+                        }
+                    }
+                }
+                return res.status(201).send(response);
             }
         )
-    });
-
-    
+    });   
 });
 
 //Retorna os dados de um produto
@@ -45,13 +66,30 @@ router.get('/:id_produto', (req, res, next) => {
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
             'SELECT * FROM produtos WHERE id_produto = ?;',
-            [re.params.id_produto],
-            (error, resultado, fields) => {
+            [req.params.id_produto],
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({response: resultado})
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        mensagem: 'Não foi econtrado produto com este ID'
+                    })
+                }
+                const response = {
+                    produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna um produto',
+                            url: 'http://localhost:3000/produtos'
+                        }
+                    }
+                }
+                return res.status(200).send(response);
             }
         )
-    })
+    });
 });
 
 //Altera um produto
@@ -68,13 +106,23 @@ router.patch('/', (req, res, next) => {
                 req.body.preco, 
                 req.body.id_produto
             ],
-            (erro, resultado, field) => {
+            (erro, result, field) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(202).send({
-                    mensagem: 'Produto alterado com sucesso',
-                });
+                const response = {
+                    mensagem: 'Produto atualizado com sucesso',
+                    produtoAtualizado: {
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna os detalhes de um produtos específico',
+                            url: 'http://localhost:3000/produtos/' + req.body.id_produto
+                        }
+                    }
+                }
+                res.status(202).send(response);
             }
         )
     });
@@ -87,13 +135,22 @@ router.delete('/', (req, res, next) => {
         conn.query(
             `DELETE FROM produtos WHERE id_produto = ?`,
             [req.body.id_produto],
-            (erro, resultado, field) => {
+            (erro, result, field) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
-
-                res.status(202).send({
+                const response = {
                     mensagem: 'Produto removido com sucesso',
-                });
+                    request: {
+                        tipo: 'POST',
+                        descricao: 'Insere um produto',
+                        url: 'http://localhost:3000/produtos',
+                        body: {
+                            nome: 'String',
+                            preco: 'Number'
+                        }
+                    }
+                }
+                return res.status(202).send(response);
             }
         )
     });
